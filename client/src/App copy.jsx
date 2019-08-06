cf055449f943633e5c2db0868bc56734c3af60b2
hulamo <hugoladislao@gmail.com>
2019-08-05 23:32:14 -0600

    Prueba sin nada

--- client/src/App copy.jsx
+++ client/src/App copy.jsx
@@ -33,7 +33,6 @@ import NewFriend from "./components/dashboard/NewFriend";
 import Files from "./components/dashboard/Files";
 import "./App.css";
 
-/*
 var renderbar = true;
 // Check for token to keep user logged in
 if (localStorage.jwtToken) {
@@ -82,13 +81,6 @@ class App extends Component {
         </Router>
       </Provider>
     );
-  }
-}
-*/
-
-class App extends Component {
-  render() {
-    return <div>Prueba</div>;
   }
 }
 
