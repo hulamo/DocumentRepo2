853e339d094e94a6e56021f5ad4c02102a246f05
hulamo <hugoladislao@gmail.com>
2019-08-07 11:28:30 -0600

    Nuevo

--- server.js
+++ server.js
@@ -29,23 +29,23 @@ const db = require("./config/keys").mongoURI;
 if (process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 
-    /*
-    }
-    
-    
-    
-    if (process.env.NODE_ENV === 'production') {
-        // Exprees will serve up production assets
-        app.use(express.static('client/build'));
-    */
 
-    // Express serve up index.html file if it doesn't recognize route
-    const path = require('path');
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
 }
 
+/*
+ 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+
+// Express serve up index.html file if it doesn't recognize route
+const path = require('path');
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
+}
+*/
 
 // Connect to MongoDB
 /*
