3ca82d9ccd6426fc84f863e01fc08e4bd01b94e7
hulamo <hugoladislao@gmail.com>
2019-08-07 23:11:18 -0600

    Arreglo de los Links de la página de Plans

--- client/src/components/layout/Plans.jsx
+++ client/src/components/layout/Plans.jsx
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Redirect } from "react-router-dom";
+
 import {
   MDBBtn,
   MDBCard,
   MDBCardBody,
-  MDBCardImage,
-  MDBCardTitle,
-  MDBCardText,
   MDBRow,
   MDBCol,
-  MDBIcon
+  MDBIcon,
+  MDBContainer
 } from "mdbreact";
 /*
 <img
@@ -32,169 +31,234 @@ import {
 */
 
 class Landing extends Component {
+  state = {
+    redirect: false,
+    linkk: "/"
+  };
+
+  setRedirectr = () => {
+    console.log("redirect");
+    this.setState({
+      redirect: true,
+      linkk: "/register"
+    });
+  };
+
+  renderRedirect = () => {
+    if (this.state.redirect) {
+      return <Redirect to={this.state.linkk} />;
+    }
+  };
+
   render() {
     return (
-      <container>
-        <MDBRow className="justify-content-md-center">
-          <MDBCol md="8">
-            <section className="text-center">
-              <h2 className="h1-responsive font-weight-bold text-center">
-                Pricing Plans
-              </h2>
-              <p
-                style={{ fontSize: 24 }}
-                className="grey-text text-center mx-auto "
-              >
-                We have the plan to suit your needs, start organizing and
-                sharing your documents now.
-              </p>
-              <MDBRow>
-                <MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
-                  <MDBCard pricing>
-                    <div className="price header white-text success-color rounded-top">
-                      <h2 className="number">0</h2>
-                      <div className="version">
-                        <h5 className="mb-0">Free</h5>
+      <div>
+        {this.renderRedirect()}
+
+        <MDBContainer>
+          <MDBRow className="justify-content-md-center">
+            <MDBCol md="8">
+              <section className="text-center">
+                <h2 className="h1-responsive font-weight-bold text-center">
+                  Pricing Plans
+                </h2>
+                <p
+                  style={{ fontSize: 24 }}
+                  className="grey-text text-center mx-auto "
+                >
+                  We have the plan to suit your needs, start organizing and
+                  sharing your documents now.
+                </p>
+                <MDBRow>
+                  <MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
+                    <MDBCard pricing>
+                      <div className="price header white-text success-color rounded-top">
+                        <h2 className="number">0</h2>
+                        <div className="version">
+                          <h5 className="mb-0">Free</h5>
+                        </div>
                       </div>
-                    </div>
-                    <MDBCardBody className="striped mb-1">
-                      <ul>
-                        <li>
-                          <p className="mt-2">
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            1 GB Of Storage
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            Unlimited Contacts
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            Unlimited shared information
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            100 Files
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="times" className="red-text pr-2" />
-                            Personal PublicRepo
-                          </p>
-                        </li>
-                      </ul>
-                      <MDBBtn href="/register" color="green">
-                        Buy now
-                      </MDBBtn>
-                    </MDBCardBody>
-                  </MDBCard>
-                </MDBCol>
-                <MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
-                  <MDBCard pricing>
-                    <div className="price header white-text success-color-dark rounded-top">
-                      <h2 className="number">10</h2>
-                      <div className="version">
-                        <h5 className="mb-0">Basic</h5>
+                      <MDBCardBody className="striped mb-1">
+                        <ul>
+                          <li>
+                            <p className="mt-2">
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              1 GB Of Storage
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              Unlimited Contacts
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              Unlimited shared information
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              100 Files
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon icon="times" className="red-text pr-2" />
+                              Personal PublicRepo
+                            </p>
+                          </li>
+                        </ul>
+                        <MDBBtn onClick={this.setRedirectr} color="green">
+                          Buy now
+                        </MDBBtn>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBCol>
+                  <MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
+                    <MDBCard pricing>
+                      <div className="price header white-text success-color-dark rounded-top">
+                        <h2 className="number">10</h2>
+                        <div className="version">
+                          <h5 className="mb-0">Basic</h5>
+                        </div>
                       </div>
-                    </div>
-                    <MDBCardBody className="striped mb-1">
-                      <ul>
-                        <li>
-                          <p className="mt-2">
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            5 GB Of Storage
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            Unlimited Contacts
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            Unlimited shared information
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            250 Files
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            Personal PublicRepo
-                          </p>
-                        </li>
-                      </ul>
-                      <MDBBtn href="/register" color="green">
-                        Buy now
-                      </MDBBtn>
-                    </MDBCardBody>
-                  </MDBCard>
-                </MDBCol>
-                <MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
-                  <MDBCard pricing>
-                    <div className="price header white-text default-color-dark rounded-top">
-                      <h2 className="number">30</h2>
-                      <div className="version">
-                        <h5 className="mb-0">Corporate</h5>
+                      <MDBCardBody className="striped mb-1">
+                        <ul>
+                          <li>
+                            <p className="mt-2">
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              5 GB Of Storage
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              Unlimited Contacts
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              Unlimited shared information
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              250 Files
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              Personal PublicRepo
+                            </p>
+                          </li>
+                        </ul>
+                        <MDBBtn onClick={this.setRedirectr} color="green">
+                          Buy now
+                        </MDBBtn>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBCol>
+                  <MDBCol lg="4" md="12" className="mb-lg-0 mb-4">
+                    <MDBCard pricing>
+                      <div className="price header white-text default-color-dark rounded-top">
+                        <h2 className="number">30</h2>
+                        <div className="version">
+                          <h5 className="mb-0">Corporate</h5>
+                        </div>
                       </div>
-                    </div>
-                    <MDBCardBody className="striped mb-1">
-                      <ul>
-                        <li>
-                          <p className="mt-2">
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            10 GB Of Storage
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            Unlimited Contacts
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            Unlimited shared information
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            500 Files
-                          </p>
-                        </li>
-                        <li>
-                          <p>
-                            <MDBIcon icon="check" className="green-text pr-2" />
-                            Corporate PublicRepo
-                          </p>
-                        </li>
-                      </ul>
-                      <MDBBtn href="/register" color="green">
-                        Buy now
-                      </MDBBtn>
-                    </MDBCardBody>
-                  </MDBCard>
-                </MDBCol>
-              </MDBRow>
-            </section>
-          </MDBCol>
-        </MDBRow>
-      </container>
+                      <MDBCardBody className="striped mb-1">
+                        <ul>
+                          <li>
+                            <p className="mt-2">
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              10 GB Of Storage
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              Unlimited Contacts
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              Unlimited shared information
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              500 Files
+                            </p>
+                          </li>
+                          <li>
+                            <p>
+                              <MDBIcon
+                                icon="check"
+                                className="green-text pr-2"
+                              />
+                              Corporate PublicRepo
+                            </p>
+                          </li>
+                        </ul>
+                        <MDBBtn onClick={this.setRedirectr} color="green">
+                          Buy now
+                        </MDBBtn>
+                      </MDBCardBody>
+                    </MDBCard>
+                  </MDBCol>
+                </MDBRow>
+              </section>
+            </MDBCol>
+          </MDBRow>
+        </MDBContainer>
+      </div>
     );
   }
 }
