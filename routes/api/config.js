615f1aa80e6278d3ab7dff532611493b1ccbf015
hulamo <hugoladislao@gmail.com>
2019-08-09 11:48:32 -0600

    Poniendo Enviroment de Google

--- routes/api/config.js
+++ routes/api/config.js
@@ -19,27 +19,27 @@ const nconf = (module.exports = require('nconf'));
 const path = require('path');
 
 nconf
-  // 1. Command-line arguments
-  .argv()
-  // 2. Environment variables
-  .env(['CLOUD_BUCKET', 'NODE_ENV', 'PORT'])
-  // 3. Config file
-  .file({file: path.join(__dirname, 'config.json')})
-  // 4. Defaults
-  .defaults({
-    // Typically you will create a bucket with the same name as your project ID.
-    CLOUD_BUCKET: '',
+// 1. Command-line arguments
+    .argv()
+    // 2. Environment variables
+    .env(['CLOUD_BUCKET', 'NODE_ENV.GOOGLE_KEY', 'PORT'])
+    // 3. Config file
+    .file({ file: path.join(__dirname, 'config.json') })
+    // 4. Defaults
+    .defaults({
+        // Typically you will create a bucket with the same name as your project ID.
+        CLOUD_BUCKET: '',
 
-    PORT: 8080,
-  });
+        PORT: 8080,
+    });
 
 // Check for required settings
 checkConfig('CLOUD_BUCKET');
 
 function checkConfig(setting) {
-  if (!nconf.get(setting)) {
-    throw new Error(
-      `You must set ${setting} as an environment variable or in config.json!`
-    );
-  }
+    if (!nconf.get(setting)) {
+        throw new Error(
+            `You must set ${setting} as an environment variable or in config.json!`
+        );
+    }
 }
